cd1e528a0cd4966c13f30ad6c78ce1d94cbe0dc6
timcrocodile <121046725+timcrocodile@users.noreply.github.com>
2022-12-21 17:18:42 +0100

    Add files via upload

---
+++ 21.12/script.js
@@ -0,0 +1,11 @@
+// # Esercizio 1
+
+// Sulla base della lezione del giorno:
+
+// - scrivere una prima funzione che presa un stringa come argomento, ritorni la stessa stringa + la parola `bootcamp`;
+
+function esercizio1(string) {
+  console.log(string + " bootcamp");
+  return string + " bootcamp";
+}
+esercizio1("ecco la stringa iniziale + ");
