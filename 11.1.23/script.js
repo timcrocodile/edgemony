2d335a888746848564ca15fc5e3c5746c34f7beb
timcrocodile <121046725+timcrocodile@users.noreply.github.com>
2023-01-11 18:52:24 +0100

    Add files via upload

---
+++ 11.1.23/script.js
@@ -0,0 +1,13 @@
+// document.getElementById("bottone").onclick = function () {
+//   document.getElementById("descrizione").style.color = "green";
+// };
+
+// perchè il codice sopra non funziona
+// ed invece se inserisco lo stesso come script nella pagina html invece si ?
+
+// const bottone = document.getElementById("bottone");
+// bottone.addEventListener("click", () => {
+//   carta.remove();
+// });
+
+// anche per questa seconda devo inserirla nell'inde
