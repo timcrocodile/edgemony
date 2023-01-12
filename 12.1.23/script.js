8cfb8f11afec8b3e86a21af1ccdccf15b5d676ae
timcrocodile <121046725+timcrocodile@users.noreply.github.com>
2023-01-12 16:45:50 +0100

    Add files via upload

---
+++ 12.1.23/script.js
@@ -0,0 +1,24 @@
+// Eseguire sull'array riportato sotto:
+
+// - un metodo forEach che stampi in console.log i soli ID di ogni singolo elemento
+// - un metodo map che memorizzi in una variabile tutti gli userId di ogni singolo elemento e quindi richiamarli con un console.log a parte
+// - un metodo filter che stampi in console.log i soli ID di ogni singolo elemento i cui valori siano maggiori o uguali a 4
+
+import { todos } from "./mock.js";
+
+// console.log(todos);
+// ho fatto console log per controllare che fosse stata importata correttamente l'array
+
+// primo punto
+// todos.forEach((obj) => console.log(obj.id));
+
+// secondo punto
+// const userIds = todos.map((todo) => todo.userId);
+// console.log(userIds);
+
+// terzo punto
+
+const filteredid = todos.filter((todo) => todo.id.length >= 4);
+console.log(filteredid);
+
+//non riesco a capire perchè mi da un array vuota, forse non interpreta bene il numero 4 la funzione che ho scritto ?
