80fbd0574a0beae9c702c655771379b19e256c12
timcrocodile <timcrocodile@proton.me>
2023-01-16 15:23:25 +0100

    aggiunta cartella recap

---
+++ recap/script.js
@@ -0,0 +1,23 @@
+const bodyEl = document.querySelector("body");
+const div = document.createElement("div");
+
+// proprietà che aggiunge una classe
+div.className = "pippo";
+
+// metodo che aggiunge altre classi
+div - classList.add("pippo2");
+
+// style in linea - quindi ha bisogno di una chiave + valore
+div.style = "background-color : black ; height : 100px ; width : 100 px ";
+
+bodyEl.append(div);
+
+const nome = "giuseppe";
+const cognome = "Senettone";
+const funzione = (parametro1, parametro2) => {
+  console.log("nome", parametro);
+  cì;
+  console.log("cognome", valore2);
+};
+
+funzione(nome, cognome);
