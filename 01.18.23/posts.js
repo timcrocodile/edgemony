e3edd1fe4631795a9025818f12a82ec46c58247f
timcrocodile <timcrocodile@proton.me>
2023-01-19 10:53:18 +0100

    mod

--- 01.18.23/posts.js
+++ 01.18.23/posts.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 const postForm = document.querySelector("#postForm");
 const inputTitle = document.querySelector("#title");
 const inputPrice = document.querySelector("#price");
@@ -39,45 +38,3 @@ const funzionePost = (objBody) => {
     .then((data) => console.log("RISPOSTA POST: ", data))
     .catch((e) => console.log("ERRORE: ", e));
 };
-=======
-const postForm = document.querySelector("#postForm");
-const inputTitle = document.querySelector("#title");
-const inputPrice = document.querySelector("#price");
-const inputCategory = document.querySelector("#category");
-const inputDescription = document.querySelector("#description");
-const inputImage = document.querySelector("#image");
-
-postForm.addEventListener("submit", (event) => {
-  // previene il comportamente di defualt del submit
-  event.preventDefault();
-
-  // nuovo oggetto popolato con i valori del form
-  const newObj = {
-    title: inputTitle.value,
-    price: parseInt(inputPrice.value),
-    description: inputDescription.value,
-    categoryId: parseInt(inputCategory.value),
-    images: [inputImage.value],
-  };
-  funzionePost(newObj);
-});
-
-const funzionePost = (objBody) => {
-  fetch("https://api.escuelajs.co/api/v1/products", {
-    method: "POST", // *GET, POST, PUT, DELETE, etc.
-    headers: {
-      "Content-Type": "application/json",
-      // 'Content-Type': 'application/x-www-form-urlencoded',
-    },
-    body: JSON.stringify(objBody), // body data type must match "Content-Type" header
-    // mode: "cors", // no-cors, *cors, same-origin
-    // cache: "no-cache", // *default, no-cache, reload, force-cache, only-if-cached
-    // credentials: "same-origin", // include, *same-origin, omit
-    // redirect: "follow", // manual, *follow, error
-    // referrerPolicy: "no-referrer", // no-referrer, *no-referrer-when-downgrade, origin, origin-when-cross-origin, same-origin, strict-origin, strict-origin-when-cross-origin, unsafe-url
-  })
-    .then((res) => res.json())
-    .then((data) => console.log("RISPOSTA POST: ", data))
-    .catch((e) => console.log("ERRORE: ", e));
-};
->>>>>>> 5b4b50094e236e6c2b7b0a2cfe8f47a4b562d4b9
