65ea610936f96e8b771c06d07d2410f4e7ba4650
timcrocodile <timcrocodile@proton.me>
2023-01-26 17:52:36 +0100

    26genn

---
+++ 26.01.2023/scipt2.js
@@ -0,0 +1,35 @@
+const bodyEl = document.body;
+
+const timeEl = document.createElement("div");
+timeEl.className = "orologio";
+bodyEl.appendChild(timeEl);
+
+let timeoutIds = [];
+
+function countDown(time, message) {
+  timeoutIds.push(
+    setTimeout(() => {
+      timeEl.textContent = message;
+    }, time)
+  );
+}
+
+countDown(1000, "3");
+countDown(2000, "2");
+countDown(3000, "1");
+countDown(4000, "questo messaggio si autodistruggerà in .....");
+
+setTimeout(() => {
+  timeEl.remove();
+  btnEl.remove();
+  document.body.style.backgroundColor = "red";
+}, 5000);
+
+const btnEl = document.createElement("button");
+btnEl.textContent = "STOP, che però non ti salverà dalla distruzione!";
+btnEl.classList.add("bottoncino");
+
+btnEl.addEventListener("click", function () {
+  timeoutIds.forEach((timeoutId) => clearTimeout(timeoutId));
+});
+bodyEl.appendChild(btnEl);
