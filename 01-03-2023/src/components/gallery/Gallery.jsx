63a390a823fcf5c19b20b2a052c297302f93d268
timcrocodile <timcrocodile@proton.me>
2023-03-01 16:42:38 +0100

    1marxxo23

--- 01-03-2023/src/components/gallery/Gallery.jsx
+++ 01-03-2023/src/components/gallery/Gallery.jsx
@@ -4,6 +4,7 @@ import Listaprodotti2 from "../listaprodotti2/Listaprodotti2";
 const Gallery = ({ listData }) => {
   return (
     <div className="nuovagalleria">
+      <p>titolo della nuova galleria</p>
       {listData.map((product) => (
         <Listaprodotti2 productData={product} key={product.id} />
       ))}
